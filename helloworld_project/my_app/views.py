89a765b84a1521438ac24527f92fa61717497bf6
Zachariah Lowe <lowezake@gmail.com>
2017-10-25 00:25:55 -0600

    py3 hw up

---
+++ helloworld_project/my_app/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+from django.shortcuts import render
+from django.views.generic import TemplateView
+class HomeView(TemplateView):
+    template_name = 'index.html'
